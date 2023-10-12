37db1ab7b1b8bcfad1010686df8d6015b4818df3
Aleks <aleksashka_yar@mail.ru>
2023-10-12 10:34:14 +0300

    first init

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName:'JS tutorial',
+        productNumber:'98734',
+        paymentStatus: 'Due',
+        status: 'Pending',
+    },
+    {
+        productName:'CSS full course',
+        productNumber:'88743',
+        paymentStatus: 'Refunded',
+        status: 'Declined',
+    },
+    {
+        productName:'Flex-Box tutorial',
+        productNumber:'33425',
+        paymentStatus: 'Paid',
+        status: 'Active',
+    },
+]
